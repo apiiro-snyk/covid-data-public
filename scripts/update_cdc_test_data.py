4dccc066987e4d9a30c3f0e0ec2031e44bda29ac
Michael Lehenbauer <michael@covidactnow.org>
2020-12-15 08:54:36 -0800

    Reference twello ticket for disabled CA county CDC test data. (#182)

--- scripts/update_cdc_test_data.py
+++ scripts/update_cdc_test_data.py
@@ -98,7 +98,7 @@ def update(data_url: str):
 
     # Remove California data. Recent test positivity values were obviously broken starting Dec 15.
     # Restore it once CDC fixes their data.
-    # See https://covidactnow.slack.com/archives/C0182MKHWCR/p1608047925064000?thread_ts=1608020222.062200&cid=C0182MKHWCR
+    # See https://trello.com/c/2hNyaCHc/678-cdc-county-test-california-showing-spurious-numbers
     data = data.loc[data[CommonFields.STATE] != "CA", :]
 
     return remove_trailing_zeros(data)
