41775a486eb9161afbe1ab833bdfb2dc9947bf5a
Chris Kelly <ghop02@gmail.com>
2021-02-10 00:50:02 -0500

    Re enable updaters and remove covid county data (#206)
    * Re-enable county updaters
    
    * Remove covid county data code
    
    * Remove test fixtures
    
    * re-add data

--- update.sh
+++ update.sh
@@ -11,19 +11,18 @@ python scripts/update_test_and_trace.py
 
 # The following all use ccd_helpers.py. The first uses --fetch and the rest can run with --no-fetch
 # to use the file saved locally.
-# Disabling updaters from CAN scraper until CAN scraper fix implemented https://trello.com/c/uHwk0PaM
-# python scripts/update_can_scraper_state_providers.py --fetch
-# python scripts/update_cdc_test_data.py --no-fetch
-# python scripts/update_cdc_vaccine_data.py --no-fetch
+python scripts/update_can_scraper_state_providers.py --fetch
+python scripts/update_cdc_test_data.py --no-fetch
+python scripts/update_cdc_vaccine_data.py --no-fetch
 
 # HHS hospital data also uses the parquet file, but pulls directly from the bucket.
-# python scripts/update_hhs_hospital_data.py
+python scripts/update_hhs_hospital_data.py
 
 # TODO(https://trello.com/c/PeQXdUCU): Fix Texas hospitalizations.
 python scripts/update_texas_tsa_hospitalizations.py || echo "Failed to update Texas Hospitals"
 python scripts/update_texas_fips_hospitalizations.py
 python scripts/update_forecast_hub.py || echo "Failed to update Forecast Hub projections"
-python scripts/update_covid_county_data.py
+
 # AWS Lake seems to be hanging the build right now.
 # python scripts/update_aws_lake.py --replace-local-mirror --cleanup-local-mirror
 python scripts/update_hhs_testing_data.py
